31fed7292c2e39679b6a5429e7c53e1448e1e713
FlutLab.io <>
2021-01-12 13:45:41 +0000

    one container

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -9,14 +9,17 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("BMI CALCULATOR"),
-        ),
-        body: Container(
+      appBar: AppBar(
+        title: Text("BMI CALCULATOR"),
+      ),
+      body: Container(
+          height: 200,
+          width: 270,
+          margin: EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
             color: Color(0xFF1D1E33),
-            height: 200, width: 270,
-            margin: EdgeInsets.all(15.0),
-        ), 
+            borderRadius: BorderRadius.circular(10.0),
+          )),
     );
   }
 }
